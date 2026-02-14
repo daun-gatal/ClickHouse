29566ed70f43ee4bfec4e3e23ba7b330a7288be6
Mikhail Artemenko <mikhail.artemenko@clickhouse.com>
2026-02-14 00:49:55 +0000

    fix profile_counters_scopes collection

--- src/Interpreters/InsertDependenciesBuilder.cpp
+++ src/Interpreters/InsertDependenciesBuilder.cpp
@@ -1101,7 +1101,7 @@ bool InsertDependenciesBuilder::observePath(const DependencyPath & path)
         inner_tables[current] = materialized_view->getTargetTableId();
         source_tables[current] = parent;
         thread_groups[current] = ThreadGroup::createForMaterializedView(init_context);
-        profile_counters_scopes[root_view] = CurrentThread::getCountersScope();
+        profile_counters_scopes[current] = CurrentThread::getCountersScope();
         view_types[current] = QueryViewsLogElement::ViewType::MATERIALIZED;
         views_error_registry->init(current);
 
@@ -1131,7 +1131,7 @@ bool InsertDependenciesBuilder::observePath(const DependencyPath & path)
         select_queries[current] = window_view->getMergeableQuery();
         input_headers[current] = output_headers.at(path.parent(2));
         thread_groups[current] = ThreadGroup::createForMaterializedView(init_context);
-        profile_counters_scopes[root_view] = CurrentThread::getCountersScope();
+        profile_counters_scopes[current] = CurrentThread::getCountersScope();
         view_types[current] = QueryViewsLogElement::ViewType::WINDOW;
         views_error_registry->init(current);
 
